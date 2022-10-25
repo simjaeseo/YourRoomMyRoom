4741dbb669d288b533f885dbf8240cbd6e01a6a0
jg6735 <jg6735@naver.com>
2022-10-25 17:34:34 +0900

    [BE] feat: Querydsl Support 클래스 생성

--- backend/business-service/src/main/java/com/footprints/businessservice/domain/transferboard/repository/TransferRepository.java
+++ backend/business-service/src/main/java/com/footprints/businessservice/domain/transferboard/repository/TransferRepository.java
@@ -1,4 +1,8 @@
 package com.footprints.businessservice.domain.transferboard.repository;
 
-public interface TransferRepository {
+import com.footprints.businessservice.domain.transferboard.entity.Transfer;
+import com.footprints.businessservice.domain.transferboard.repository.custom.TransferRepositoryCustom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferRepository extends JpaRepository<Transfer, Long>, TransferRepositoryCustom {
 }
---
+++ backend/business-service/src/main/java/com/footprints/businessservice/domain/transferboard/repository/custom/TransferRepositoryCustom.java
@@ -0,0 +1,4 @@
+package com.footprints.businessservice.domain.transferboard.repository.custom;
+
+public interface TransferRepositoryCustom {
+}
