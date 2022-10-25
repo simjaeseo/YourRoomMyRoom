863f4691b7952db2624ca9124bd6a2a1621cabce
jg6735 <jg6735@naver.com>
2022-10-25 17:34:10 +0900

    [BE] feat: health check

--- backend/business-service/src/main/java/com/footprints/businessservice/domain/transferboard/api/TransferControllerV1.java
+++ backend/business-service/src/main/java/com/footprints/businessservice/domain/transferboard/api/TransferControllerV1.java
@@ -1,4 +1,23 @@
 package com.footprints.businessservice.domain.transferboard.api;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@Slf4j
 public class TransferControllerV1 {
+
+    private final Environment env;
+
+    @GetMapping("/health_check")
+    public String check() {
+        log.info("Server.port={}", env.getProperty("local.server.port"));
+        return String.format("This Service port is %s", env.getProperty("local.server.port"));
+    }
 }
