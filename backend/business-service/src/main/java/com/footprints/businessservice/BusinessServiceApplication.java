d9ef0484d5e8e170b78b529c9757e981f718fc4e
jg6735 <jg6735@naver.com>
2022-10-25 15:01:59 +0900

    [BE] feat: Spring Cloud Eureka Server, Clients 등록

--- backend/business-service/src/main/java/com/footprints/businessservice/BusinessServiceApplication.java
+++ backend/business-service/src/main/java/com/footprints/businessservice/BusinessServiceApplication.java
@@ -2,8 +2,14 @@ package com.footprints.businessservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableDiscoveryClient
+@EnableFeignClients
 public class BusinessServiceApplication {
 
     public static void main(String[] args) {
