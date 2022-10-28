b2359a020d76ec9dc2cd3b4a7cec8656ade9bd65
simjaeseo <delpho@naver.com>
2022-10-28 17:53:23 +0900

    [BE] feat: refresh token 재발급 api

--- backend/auth-service/src/main/java/com/footprints/authservice/service/AuthService.java
+++ backend/auth-service/src/main/java/com/footprints/authservice/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.footprints.authservice.service;
 
 import com.footprints.authservice.global.jwt.TokenProvider;
+import com.footprints.authservice.global.redis.RedisService;
 import com.footprints.authservice.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ public class AuthService {
 
     private final TokenProvider tokenProvider;
     private final MemberRepository memberRepository;
+    private final RedisService redisService;
 
     public void logout(String accessToken) {
         String providerId = tokenProvider.getMemberProviderId(accessToken);
@@ -23,4 +25,28 @@ public class AuthService {
     }
 
 
+    public String reissueRefreshToken(String memberId, String refreshToken) {
+
+        // RT 유효성 검사
+        if(!tokenProvider.validateToken(refreshToken)){
+            return null;
+        }
+
+        // redis에 저장된 토큰이 맞는지 확인
+        String memberRefreshToken = redisService.getValue(memberId);
+
+        // redis에 저장된 토큰이 맞는지 확인
+        if(memberRefreshToken != refreshToken){
+            return null;
+        }
+
+        // AT 재발급
+        String newRefreshToken = tokenProvider.createRefreshToken(Long.parseLong(memberId));
+
+        // redis에 새로 저장
+        redisService.setValues(memberId, newRefreshToken);
+
+        return newRefreshToken;
+
+    }
 }
