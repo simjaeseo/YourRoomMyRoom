87de6a2de9185de5da72286450d970625f8cd1e1
jg6735 <jg6735@naver.com>
2022-10-28 17:35:19 +0900

    [BE] feat: 사진 엔티티 생성

--- backend/business-service/src/main/java/com/footprints/businessservice/domain/board/image/entity/Image.java
+++ backend/business-service/src/main/java/com/footprints/businessservice/domain/board/image/entity/Image.java
@@ -1,4 +1,36 @@
 package com.footprints.businessservice.domain.board.image.entity;
 
-public class Image {
+import com.footprints.businessservice.domain.board.article.entity.Article;
+import com.footprints.businessservice.global.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Image extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    private String fileName;
+
+    private String originalName;
+
+    private String url;
+
+    private Long size;
 }
