f15b353edf23d42fbed719d36d4a14b2e18b47f1
ksmhero <ksmhero@naver.com>
2022-11-08 23:51:11 +0900

    [BE] feat: 채팅 기능 구현 중

--- backend/business-service/src/main/java/com/footprints/businessservice/app/domain/chat/api/ChatController.java
+++ backend/business-service/src/main/java/com/footprints/businessservice/app/domain/chat/api/ChatController.java
@@ -3,6 +3,7 @@ package com.footprints.businessservice.app.domain.chat.api;
 import com.footprints.businessservice.app.domain.chat.dto.ChatRoomRequest;
 import com.footprints.businessservice.app.domain.chat.entity.ChatRoom;
 import com.footprints.businessservice.app.domain.chat.service.ChatService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,7 @@ public class ChatController {
     private final ChatService chatService;
 
     @PostMapping
+    @Operation(summary = "채팅방 생성")
     public void createRoom(@RequestHeader(name = "X-Authorization-Id") String memberId, @RequestBody ChatRoomRequest request) {
         chatService.createRoom(memberId, request);
     }
