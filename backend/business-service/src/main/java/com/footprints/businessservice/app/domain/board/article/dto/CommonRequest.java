db045a2e77f0450a2fe7dd8c74d6f60bcb064a61
jg6735 <jg6735@naver.com>
2022-11-16 15:17:02 +0900

    [BE] refactor: 게시글 작성 요청 양식 변경

--- backend/business-service/src/main/java/com/footprints/businessservice/app/domain/board/article/dto/CommonRequest.java
+++ backend/business-service/src/main/java/com/footprints/businessservice/app/domain/board/article/dto/CommonRequest.java
@@ -1,15 +1,26 @@
 package com.footprints.businessservice.app.domain.board.article.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.footprints.businessservice.app.domain.board.transfer.dto.TransferRequest;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommonRequest {
 
     private ArticleRequest articleRequest;
 
     private TransferRequest transferRequest;
 
+    public CommonRequest(ArticleRequest articleRequest) {
+        this.articleRequest = articleRequest;
+    }
+
+    public CommonRequest(TransferRequest transferRequest) {
+        this.transferRequest = transferRequest;
+    }
 }
